67bea6acd561898323ee915af5f91146c1463368
Andrii Kicha <akicha@cogniance.com>
2018-12-13 20:56:33 +0200

    My Portfolio

---
+++ precache-manifest.3a7585f81107043d17fe695ad8732ad1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ad05c4ca8d30ef2595d7a1b16edc803f",
+    "url": "/static/media/me.ad05c4ca.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c1172fa97e8612b8b289",
+    "url": "/static/js/main.c1172fa9.chunk.js"
+  },
+  {
+    "revision": "6dd950665eb7e4331f37",
+    "url": "/static/js/1.6dd95066.chunk.js"
+  },
+  {
+    "revision": "c1172fa97e8612b8b289",
+    "url": "/static/css/main.5b5f3ead.chunk.css"
+  },
+  {
+    "revision": "6dd950665eb7e4331f37",
+    "url": "/static/css/1.5cb8ec48.chunk.css"
+  },
+  {
+    "revision": "9033b3773713fe33ab87f894296b4c23",
+    "url": "/index.html"
+  }
+];
